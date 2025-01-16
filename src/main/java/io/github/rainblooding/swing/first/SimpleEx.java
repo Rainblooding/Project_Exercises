32f8f9fb9e2cad703c3b825e3f293764ef8031a6
rainblooding <2392575187@qq.com>
2025-01-16 22:34:23 +0800

    添加按钮

--- src/main/java/io/github/rainblooding/swing/first/SimpleEx.java
+++ src/main/java/io/github/rainblooding/swing/first/SimpleEx.java
@@ -16,10 +16,29 @@ public class SimpleEx extends JFrame {
 
 
     public void initUI() {
+        var quitButton = new JButton("Quit");
+        quitButton.addActionListener((event) -> System.exit(0));
+        this.createLayout(quitButton);
+
         this.setTitle("Simple example");
         this.setSize(300, 200);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void createLayout(JComponent... arg) {
+        var pane = this.getContentPane();
+        var gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+        gl.setAutoCreateContainerGaps(true);
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
     }
 
     public static void main(String[] args) {
